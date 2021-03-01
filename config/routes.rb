0507e7cc3ce8fa4cc0f2b000913d019bf8793a89
romanolivi <romano_livi@outlook.com>
2021-03-01 12:25:49 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get "/", to: "products#index", as: "homepage"
+
+  post "/add", to: "products#add"
+
+  # post "/products/add", to: "products#add", as: "add"
+
 end
